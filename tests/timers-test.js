aaa69f0163821c7d66ee4b6e6b61f603d3a390ac
Shane Tomlinson <set117@yahoo.com>
2011-09-11 15:07:59 -0700

    Adding the basic ability to start and stop.

--- tests/timers-test.js
+++ tests/timers-test.js
@@ -1,6 +1,46 @@
 (function() {
   module("timers");
 
+  var timers = Timers;
+
+  test("start", function() {
+    var id = timers.start();
+    equal(typeof id, 'number', "start successful, we have an id");
+
+    var id2 = timers.start();
+    equal(typeof id, 'number', "start successful, we have a second id");
+
+    notEqual(id, id2, "The two ids are not equal");
+  });
+
+  test("stop with valid timer", function() {
+    var id = timers.start();
+    var elapsed = timers.stop(id);
+
+    equal(typeof elapsed, "number", "elapsed time is a number");
+    ok(0 <= elapsed && elapsed < 100, "elapsed time is > 0");
+  });
+
+  test("stop after async timeout", function() {
+    var id = timers.start();
+
+    setTimeout(function() {
+      var elapsed = timers.stop(id);
+
+      // allow for timers firing slightly early
+      ok(elapsed > 990, "elapsed time is around what we figured");
+      start();
+    }, 1000);
+
+    stop();
+  });
+
+  test("stop with invalid timer", function() {
+    var id = timers.start();
+    raises(function() {
+      timers.stop("a");
+    }, "Invalid timer", "stop with invalid timer");
+  });
 
 
 }());
--- timers.js
+++ timers.js
@@ -1,4 +1,30 @@
 var Timers = (function() {
 
+  var timers = {};
+  var currId = 0;
+
+
+  function start() {
+    var id = currId;
+    timers[id] = new Date().getTime();
+    currId++;
+    return id;
+  }
+
+  function stop(id) {
+    var start = timers[id];
+    if (start) {
+      return new Date().getTime() - start;
+    }
+    else {
+      throw "Invalid timer";
+    }
+  }
+
+  return {
+    start: start,
+    stop: stop
+  };
+
 }());
 
